5c893ac477b2cab83ae267330a9a39cf011b73ad
Francaios <donnarif@outlook.com>
2023-12-21 20:00:49 -0300

    me parece que esto ya lo habia intentado

--- api/header.php
+++ api/header.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../api');
+$dotenv = Dotenv\Dotenv::createImmutable('../'.__DIR__);
 $dotenv->load();
 
 session_start();
