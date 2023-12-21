cda97c1a696e03745ce698951b44ae0ef5cbd4ba
Francaios <donnarif@outlook.com>
2023-12-21 14:38:15 -0300

    Cambio de las variables de entorno y la solicitud de conexion a la db para que funcione el deploy

--- connection.php
+++ connection.php
@@ -1,32 +1,33 @@
-<?php 
+<?php
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-class connection{
-    private $server;
+class Connection {
+    private $host;
     private $username;
     private $password;
     private $connection;
     private $dbname;
+    private $port;
 
     public function __construct(){
-        $this->server = $_ENV['DB_SERVER'];
-        $this->username = $_ENV['DB_USERNAME'];
-        $this->password = $_ENV['DB_PASSWORD'];
-        $this->dbname = $_ENV['DB_NAME'];
+        $this->host = $_ENV['MYSQLHOST'];
+        $this->username = $_ENV['MYSQL_USER'];
+        $this->password = $_ENV['MYSQL_PASSWORD'];
+        $this->dbname = $_ENV['MYSQL_DATABASE'];
+        $this->port = $_ENV['MYSQL_PORT'];
 
-        try{
-            $this->connection= new PDO("mysql:host=$this->server;dbname=$this->dbname",$this->username,$this->password);
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-        }
-        catch(PDOException $e){
-            return "Connection Error".$e;
+        try {
+            $dsn = "mysql:host=$this->host;port=$this->port;dbname=$this->dbname";
+            $this->connection = new PDO($dsn, $this->username, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Connection Error: " . $e->getMessage();
         }
     }
-
 
     public function execute($sql){
         $this->connection->exec($sql);
@@ -39,10 +40,5 @@ class connection{
         return $req->fetchAll();
     }
 }
-
-
-
-
-
 
 ?>
