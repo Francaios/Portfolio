8c5a8c7956c555da4210822cd61ccdcb2a89dcff
Francaios <donnarif@outlook.com>
2023-12-21 22:18:14 -0300

    Volvemos atras para probar unas cosas

--- api/header.php
+++ api/header.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 
 session_start();
 
@@ -13,23 +13,26 @@ session_start();
     <title>Bootstrap</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-    <br />
+    <br/>
     <div class="container text-center">
         <div class="row justify-content-between">
-            <a class="col-3 btn btn-primary" href="/index.php">Inicio</a>
-            <a class="col-3 btn btn-info" href="/projects.php">Proyectos</a>
-            <a class="col-3 btn btn-secondary" href="/contact.php">Contactame</a>
+            <a class="col-3 btn btn-primary" href="index.php">Inicio</a>
+            <a class="col-3 btn btn-info" href="projects.php">Proyectos</a>
+            <a class="col-3 btn btn-secondary" href="contact.php">Contactame</a>
             <?php
             if (isset($_SESSION['user'])) {
                 echo '<a class="col-2 btn btn-danger" href="logOut.php">Cerrar Sesión</a>';
             } else {
                 echo '<a class="col-2 btn btn-primary" href="logIn.php">Iniciar Sesión</a>';
             }
-
-            ?>
-
+            
+        ?>
+        
         </div>
         </br>
+
+        
