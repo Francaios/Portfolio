cbc9c4f7a7991eb6b1d656ae05f92bec8a82fe2c
Francaios <donnarif@outlook.com>
2023-12-24 13:28:25 -0300

    Este si es el ultimo, me tengo confianza

--- api/index.php
+++ api/index.php
@@ -1,10 +1,29 @@
 <?php include("header.php"); ?>
 
 <div class="row align-items-md-stretch">
+    <div class="col-md-8">
+        <h1 class="text-primary">Bienvenidos a mi Portfolio</h1>
+        <div class="col-md-4">
+            <h3 class="text-secondary">
+                ¡Hola! Soy Francisco Donnari, un apasionado desarrollador Fullstack y Back-End con formación de técnico
+                en electrónica y actualmente
+                estudiando ingeniería en informática.
+
+            </h3>
+        </div>
+        <div class="col-md-4">
+
+            <h3 class="text-secondary">Busco contribuir al crecimiento de un equipo y aplicar al máximo mis
+                conocimientos en el desarrollo de software. Estoy emocionado por conectar y explorar oportunidades para
+                colaborar en proyectos interesantes. ¡No dudes en contactarme para discutir posibles colaboraciones o
+                ideas innovadoras!</h3>
+
+        </div>
+    </div>
     <div class="col-md-4">
-    <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703432345/Donnari-Francisco.png"
+        <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703432345/Donnari-Francisco.png"
             class="img-fluid rounded" alt="" />
-            <h2 class="text-secondary">Tengo conocimiento en</h2>
+        <h2 class="text-secondary">Tengo conocimiento en</h2>
         <?php foreach ($knowledges as $tecnologia => $valor) { ?>
             <div class="d-inline-block mr-3 mb-3">
                 <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703262003/icons/<?php echo $tecnologiasIconos[$valor]; ?>"
@@ -15,19 +34,6 @@
             </div>
         <?php } ?>
     </div>
-    <div class="col-md-4">
-    <h1 class="text-primary">Bienvenidos a mi Portfolio</h1>
-        <h3 class="text-secondary">
-            ¡Hola! Soy Francisco Donnari, un apasionado desarrollador Fullstack y Back-End con formación de técnico en electrónica y actualmente
-            estudiando ingeniería en informática.
-            
-        </h3>
-    </div>
-    <div class="col-md-4">
-    <h3 class="text-secondary">Busco contribuir al crecimiento de un equipo y aplicar al máximo mis conocimientos en el desarrollo de software. Estoy emocionado por conectar y explorar oportunidades para colaborar en proyectos interesantes. ¡No dudes en contactarme para discutir posibles colaboraciones o ideas innovadoras!</h3>
-    </div>
 </div>
 
 <?php include("footer.php"); ?>
-
-<h3 class="text-secondary">Busco contribuir al crecimiento de un equipo y aplicar al máximo mis conocimientos en el desarrollo de software. Estoy emocionado por conectar y explorar oportunidades para colaborar en proyectos interesantes. ¡No dudes en contactarme para discutir posibles colaboraciones o ideas innovadoras!</h3>
