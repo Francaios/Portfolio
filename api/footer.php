986bde1018ebcf2981a704710c3295937848bf4c
Francaios <donnarif@outlook.com>
2023-12-24 10:47:10 -0300

    Agregue datos al footer

--- api/footer.php
+++ api/footer.php
@@ -1,4 +1,30 @@
 </div>
-</body>
+<footer style="background-color: #f0f0f0; color: #333; padding: 10px;">
+<div class="container">
+    Redes sociales y otras formas de contacto
+        <div class="row">
+            <div class="col-md-4">
+                <a href="https://www.linkedin.com/in/donnari-francisco/" class="btn btn-primary btn-block" target="_blank">
+                    <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/github.png" alt="LinkedIn Icon" class="img-fluid">
+                    LinkedIn
+                </a>
+            </div>
 
+            <div class="col-md-4">
+                <a href="https://github.com/Francaios" class="btn btn-secondary btn-block" target="_blank">
+                    <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/github.png" alt="GitHub Icon" class="img-fluid">
+                    GitHub
+                </a>
+            </div>
+
+            <div class="col-md-4">
+                <a href="mailto:donnarif@outlook.com" class="btn btn-info btn-block">
+                    <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/outlook.png" alt="Correo Icon" class="img-fluid">
+                    Correo
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
+</body>
 </html>
