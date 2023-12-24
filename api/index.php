4de3bc535dd1ad9f2c639b4a84e149766a0bf4db
Francaios <donnarif@outlook.com>
2023-12-24 12:09:41 -0300

    Cambios en el index

--- api/index.php
+++ api/index.php
@@ -3,12 +3,19 @@
 <div class="row align-items-md-stretch">
     <div class="col-md-6">
         <h1 class="text-primary">Bienvenidos a mi Portfolio</h1>
-        <h3 class="text-secondary">Soy Francisco Donnari</h3>
-        <h3 class="text-secondary">Desarrollador Fullstack</h3>
-        <h3 class="text-secondary">Desarrollador Backend</h3>
+        <img
+            src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703430370/Donnari-Francisco.png"
+            class="img-fluid rounded-top"
+            alt=""
+        />
+        <p class="text-secondary">
+            Busco contribuir al crecimiento de un equipo y aplicar al máximo mis conocimientos en el desarrollo de software. Estoy emocionado por conectar y explorar oportunidades para colaborar en proyectos interesantes. ¡No dudes en contactarme para discutir posibles colaboraciones o ideas innovadoras!
+        </p>
     </div>
     <div class="col-md-6">
-
+        <h3 class="text-secondary">
+            ¡Hola! Soy Francisco Donnari, un apasionado desarrollador con formación técnica en electrónica y actualmente estudiando ingeniería informática. Mi descubrimiento y pasión por la programación me llevaron a completar con éxito un curso de Full Stack Developer, donde dominé tecnologías como JavaScript, Node.js, React, Redux, Express, Sequelize, y PostgreSQL.
+        </h3>
     </div>
 </div>
 
