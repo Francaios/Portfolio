503ad18cabb354c112d5bfb0e312255f678956c4
Francaios <donnarif@outlook.com>
2023-12-24 13:41:42 -0300

    Rounded a los background

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@
     <div class="col-md-8">
         <h1 class="text-primary">Bienvenidos a mi Portfolio</h1>
         <div class="row">
-            <div class="col-md-6 bg-secondary">
+            <div class="col-md-6 bg-secondary rounded">
                 <h3 class="text-info">
                     ¡Hola! Soy Francisco Donnari, un apasionado desarrollador Fullstack y Back-End con formación de
                     técnico
@@ -13,7 +13,7 @@
 
                 </h3>
             </div>
-            <div class="col-md-6 bg-secondary">
+            <div class="col-md-6 bg-secondary rounded">
 
                 <h3 class="text-info">Busco contribuir al crecimiento de un equipo y aplicar al máximo mis
                     conocimientos en el desarrollo de software. Estoy emocionado por conectar y explorar oportunidades
@@ -28,12 +28,12 @@
     <div class="col-md-4">
         <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703432345/Donnari-Francisco.png"
             class="img-fluid rounded bg-secondary" alt="" />
-        <h2 class="text-white bg-secondary">Tengo conocimiento en</h2>
+        <h2 class="text-white bg-secondary rounded">Tengo conocimiento en</h2>
         <?php foreach ($knowledges as $tecnologia => $valor) { ?>
-            <div class="d-inline-block mr-3 mb-3 bg-secondary">
+            <div class="d-inline-block mr-3 mb-3 rounded bg-secondary">
                 <img src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703262003/icons/<?php echo $tecnologiasIconos[$valor]; ?>"
                     alt="<?php echo $valor; ?>" width="30" height="30" class="img-fluid rounded bg-white">
-                <span class="text-white p-2">
+                <span class="text-white p-2 rounded">
                     <?php echo $tecnologiasNombres[$valor]; ?>
                 </span>
             </div>
