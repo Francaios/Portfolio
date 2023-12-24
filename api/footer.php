7fe54d8aa909eff5a11a1172d5d7413265322b24
Francaios <donnarif@outlook.com>
2023-12-24 11:21:14 -0300

    Correccion de estilos e imagen de mi cv

--- api/footer.php
+++ api/footer.php
@@ -1,40 +1,50 @@
 </div>
-<footer style="background-color: #f0f0f0; color: #333; padding: 10px; position: absolute; bottom: 0; width: 100%;">
-<div class="container">
+<footer style=" padding: 10px; position: absolute; bottom: 0; width: 100%;">
+    <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <a href="https://www.linkedin.com/in/donnari-francisco/" class="btn btn-primary btn-block" target="_blank">
-                    <img style="width: 30px; height: auto;" src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/linkedin.png" alt="LinkedIn Icon" class="img-fluid">
+                <a href="https://www.linkedin.com/in/donnari-francisco/" class="btn btn-primary btn-block"
+                    target="_blank">
+                    <img style="width: 30px; height: auto;"
+                        src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/linkedin.png"
+                        alt="LinkedIn Icon" class="img-fluid">
                     LinkedIn
                 </a>
             </div>
 
             <div class="col-md-3">
                 <a href="https://github.com/Francaios" class="btn btn-secondary btn-block" target="_blank">
-                    <img style="width: 30px; height: auto;" src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/github.png" alt="GitHub Icon" class="img-fluid">
+                    <img style="width: 30px; height: auto;"
+                        src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/github.png"
+                        alt="GitHub Icon" class="img-fluid">
                     GitHub
                 </a>
             </div>
 
             <div class="col-md-3">
                 <a href="mailto:donnarif@outlook.com" class="btn btn-info btn-block">
-                    <img style="width: 30px; height: auto;" src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/outlook.png" alt="Correo Icon" class="img-fluid">
+                    <img style="width: 30px; height: auto;"
+                        src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/outlook.png"
+                        alt="Correo Icon" class="img-fluid">
                     Correo
                 </a>
             </div>
             <div class="col-md-3">
-            <button class="btn btn-success btn-block" onclick="downloadCV()">
-                        <img style="width: 30px; height: auto;" src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/cv.png" alt="CV Icon" class="img-fluid">
-                        Mi CV
-                    </button>
+                <button class="btn btn-success btn-block" onclick="downloadCV()">
+                    <img style="width: 30px; height: auto;"
+                        src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703424858/icons/pdf.png" alt="CV Icon"
+                        class="img-fluid">
+                    Mi CV
+                </button>
             </div>
         </div>
     </div>
 </footer>
 <script>
-        function downloadCV() {
-            window.location.href = 'https://drive.google.com/uc?export=download&id=1Ge36GstTFnLsMOvERL2eh1iLpbE8s5XD';
-        }
-    </script>
+    function downloadCV() {
+        window.location.href = 'https://drive.google.com/uc?export=download&id=1Ge36GstTFnLsMOvERL2eh1iLpbE8s5XD';
+    }
+</script>
 </body>
+
 </html>
