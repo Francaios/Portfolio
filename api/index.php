d4207c734a51b7dd9ec68e8e5c291935f7a6290d
Francaios <donnarif@outlook.com>
2023-12-24 12:47:36 -0300

    Otros cambio al index

--- api/index.php
+++ api/index.php
@@ -2,9 +2,9 @@
 
 <div class="row align-items-md-stretch">
     <div class="col-md-6">
-        <h1 class="display-3">Bienvenidos a mi Portfolio</h1>
+        <h1 class="text-primary">Bienvenidos a mi Portfolio</h1>
         <h3 class="text-secondary">
-            ¡Hola! Soy Francisco Donnari, un apasionado desarrollador con formación técnica en electrónica y actualmente estudiando ingeniería informática. Mi descubrimiento y pasión por la programación me llevaron a completar con éxito un curso de Full Stack Developer, donde dominé tecnologías como JavaScript, Node.js, React, Redux, Express, Sequelize, y PostgreSQL.
+            ¡Hola! Soy Francisco Donnari, un apasionado desarrollador con formación técnica en electrónica y actualmente estudiando ingeniería informática.
         </h3>
     </div>
     <div class="col-md-6">
