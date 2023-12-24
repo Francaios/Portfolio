6a95a7fe8e2bd450d63141da1620a80eb9e19a2d
Francaios <donnarif@outlook.com>
2023-12-24 12:18:27 -0300

    Unoscambios de estilo en index

--- api/index.php
+++ api/index.php
@@ -2,20 +2,17 @@
 
 <div class="row align-items-md-stretch">
     <div class="col-md-6">
-        <h1 class="text-primary">Bienvenidos a mi Portfolio</h1>
-        <img
-            src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703430370/Donnari-Francisco.png"
-            class="img-fluid rounded-top"
-            alt=""
-        />
-        <p class="text-secondary">
-            Busco contribuir al crecimiento de un equipo y aplicar al máximo mis conocimientos en el desarrollo de software. Estoy emocionado por conectar y explorar oportunidades para colaborar en proyectos interesantes. ¡No dudes en contactarme para discutir posibles colaboraciones o ideas innovadoras!
-        </p>
-    </div>
-    <div class="col-md-6">
+        <h1 class="display-3">Bienvenidos a mi Portfolio</h1>
         <h3 class="text-secondary">
             ¡Hola! Soy Francisco Donnari, un apasionado desarrollador con formación técnica en electrónica y actualmente estudiando ingeniería informática. Mi descubrimiento y pasión por la programación me llevaron a completar con éxito un curso de Full Stack Developer, donde dominé tecnologías como JavaScript, Node.js, React, Redux, Express, Sequelize, y PostgreSQL.
         </h3>
+    </div>
+    <div class="col-md-6">
+    <img
+            src="https://res.cloudinary.com/ddev9dsdl/image/upload/v1703430370/Donnari-Francisco.png"
+            class="img-fluid rounded-top"
+            alt=""
+        />        
     </div>
 </div>
 
